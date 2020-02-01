b75c1a90df94ab6f4564af9cea7f18a758de2b12
unknown <m.azizulcse@gmail.com>
2020-02-01 20:32:25 +0600

    changed

--- database/migrations/2020_02_01_123226_create_replies_table.php
+++ database/migrations/2020_02_01_123226_create_replies_table.php
@@ -17,9 +17,9 @@ class CreateRepliesTable extends Migration
             $table->increments('id');
             $table->text('body');
 
-            $table->integer(question_id)->unsigned();
-            // $table->integer(user_id)->unsigned();
-            $table->integer(user_id);
+            $table->integer('question_id')->unsigned();
+            // $table->integer('user_id')->unsigned();
+            $table->integer('user_id');
 
             $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
