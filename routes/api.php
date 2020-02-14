2463a1d43a4d9f9a448c2e37f1e48a57e9965d06
unknown <m.azizulcse@gmail.com>
2020-02-14 23:34:15 +0600

    Like and Dislike

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,7 @@ Route::apiResource('/question','QuestionController');
 Route::apiResource('/category','CategoryController');
 
 Route::apiResource('/question/{question}/reply','ReplyController');
+
+Route::post('/like/{reply}','LikeController@likeIt');
+
+Route::delete('/like/{reply}','LikeController@unLikeIt');
